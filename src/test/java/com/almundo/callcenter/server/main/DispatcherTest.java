653511c7154221481aaf4efa23a6e58a76d60b25
Martin <martin.ramil@zentricx.com>
2019-03-27 12:49:41 -0300

    test

--- src/test/java/com/almundo/callcenter/server/main/DispatcherTest.java
+++ src/test/java/com/almundo/callcenter/server/main/DispatcherTest.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.almundo.callcenter.beans.Call;
+import com.almundo.callcenter.beans.CallStatus;
 import com.almundo.callcenter.beans.Constants;
 import com.almundo.callcenter.beans.Employee;
 import com.almundo.callcenter.beans.EmployeeStatus;
@@ -45,7 +46,9 @@ public class DispatcherTest {
 	
 	private Boolean isTerminateAllCall = Boolean.TRUE;
 	
-     /** Test dispatch 10 calls concurrently to employees.
+	private int amountFinishCalls = 0;
+	
+     /** Test dispatch calls to employees.
 	 *
 	 * @throws InterruptedException This method throws InterruptedException if interrupted while sleeping.
 	 *             
@@ -82,16 +85,18 @@ public class DispatcherTest {
         //This blocks the thread until all tasks complete their execution or the specified timeout is reached:dispatcher
         while(true){
         	this.isTerminateAllCall = Boolean.TRUE;
+        	int amountFinishCalls = 0;
         	//Returns the approximate total number of tasks that have completed execution. Because the states of tasks and threads may change 
         	//dynamically during computation, the returned value is only an approximation, but one that does not ever decrease across successive calls.
 	        if (Long.valueOf(dispatcher.getExecutorServiceCall().getCompletedTaskCount()).intValue() == Constants.CALL_AMOUNT){
 	        	//I'm going to do the second validation to check that there are no busy employees
-	        	dispatcher.getQueryEmployeesDTO().forEach(e -> {
-	        		if(e.getEmployeeStatus().equals(EmployeeStatus.BUSY)){
-	        			isTerminateAllCall = Boolean.FALSE;
+	        	dispatcher.getQueryCallsDTO().forEach(c -> {
+	        		if(c.getCallStatus().equals(CallStatus.FINISHED)){
+	        			this.amountFinishCalls++;
+	        			
 	        		}
 	        	});
-	        	if (isTerminateAllCall){
+	        	if (this.amountFinishCalls == Constants.CALL_AMOUNT){
 	        		//If the number of concurrent calls is equal to the number of calls answered by each employee, the test ran correctly
 	        		assertEquals(Constants.CALL_AMOUNT,employeeList.stream().mapToInt(employee -> employee.getAttendedCalls().size()).sum());
 	        		logger.info("Finish testDispatchTenCallsToEmployees concurrent concurrently");
@@ -102,8 +107,6 @@ public class DispatcherTest {
 	        }
         }
     }
-    
-    
     
     
      /** Execute calls in same moment, through the ThreadPoolExecutor that the Dispatcher (Call Center) 
